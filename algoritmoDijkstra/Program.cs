0e78a31793622ec0a5a716dc0ff40ba7f47cb1b8
caslini.20353 <caslini.20353@E2-9-PC20>
2024-02-15 08:30:20 +0100

    generazione grafo.

--- algoritmoDijkstra/Program.cs
+++ algoritmoDijkstra/Program.cs
@@ -1,28 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace algoritmoDijkstra
+class Dijkstra
 {
-    internal class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Random rand = new Random();
+        int m = rand.Next(5, 10);
+        int[,] matrice = new int[m, m];
+
+
+        for (int i = 0; i < m; i++)
         {
+            for (int j = i + 1; j < m; j++)
+            {
+                matrice[i, j] = rand.Next(0, 10);
+            }
+        }
+
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = i + 1; j < m; j++)
+            {
+                matrice[j, i] = matrice[i, j];
+            }
+        }
+        Console.WriteLine("Grafo generato :");
+        Console.Write(" " + " ");
 
 
 
-
-
-
-
-
-
-
-
-
+        for (int i = 0; i < m; i++)
+        {
+            Console.Write((char)('A' + i) + " ");
 
         }
+        Console.WriteLine();
+
+        for (int i = 0; i < m; i++)
+        {
+            Console.Write((char)('A' + i) + " ");
+            for (int j = 0; j < m; j++)
+            {
+                Console.Write(matrice[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+
+
+
+
     }
+
+
+
 }
